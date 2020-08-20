09fac73b932d8551813d27d1feb0abc78b2caf55
Gwaredd <gwaredd@hotmail.com>
2020-08-20 19:23:50 +0100

    animated textures

--- Assets/Scenes/AnimatedTextures.cs
+++ Assets/Scenes/AnimatedTextures.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 public class AnimatedTextures : MonoBehaviour
 {
     public string Filename;
-    public Texture Tex;
+
+    private Texture2D[] mFrames;
+    private float[]     mFrameDelay;
+
+    private int         mCurFrame = 0;
+    private float       mTime = 0.0f;
 
     void Start()
     {
@@ -16,48 +21,42 @@ public class AnimatedTextures : MonoBehaviour
         var path  = Path.Combine( Application.streamingAssetsPath, Filename );
         var gif   = MG.GIF.Decoder.Parse( File.ReadAllBytes( path ) );
 
-        var tex = new Texture2D( gif.Width, gif.Height, TextureFormat.ARGB32, false );
-        tex.filterMode = FilterMode.Point;
-        tex.wrapMode = TextureWrapMode.Clamp;
+        var numFrames = gif.NumFrames;
 
-        var img = gif.GetFrame( 0 );
+        if( numFrames == 0 )
+        {
+            return;
+        }
 
-        tex.SetPixels32( img.RawImage );
+        mFrames       = new Texture2D[ numFrames ];
+        mFrameDelay   = new float[ numFrames ];
 
-        //var p = new Color32[ S * S ];
+        for( int i=0; i < numFrames; i++ )
+        {
+            var frame       = gif.GetFrame( i );
+            mFrames[i]      = frame.CreateTexture();
+            mFrameDelay[i]  = frame.Delay / 100.0f;
+        }
 
-        //for( int i=0; i < S * S; i++ )
-        //{
-        //    byte r = (byte) Random.Range( 0, 255 );
-        //    byte g = (byte) Random.Range( 0, 255 );
-        //    byte b = (byte) Random.Range( 0, 255 );
-        //    p[i] = new Color32( r, g, b, 0xFF );
-        //}
-        tex.Apply();
-
-
-
-        //Debug.Log( $"{gif.Width}x{gif.Height} #{gif.Images.Count}" );
-
-        //var texture = new Texture2D( 128, 128, TextureFormat.ARGB32, false );
-
-        //for(int y=0; y > 128; y++ )
-        //{
-        //    for( int x=0; x < 128; x++ )
-        //    {
-        //        texture.SetPixel( x, y, Color.red );
-        //    }
-        //}
-
-        //texture.Apply( false );
-
-        GetComponent<Renderer>().material.mainTexture = tex;
-        //GetComponent<Renderer>().material.SetTexture( "_MainTex", Tex );
-
+        GetComponent<Renderer>().material.mainTexture = mFrames[0];
     }
 
     void Update()
     {
+        if( mFrames == null )
+        {
+            return;
+        }
+
+        mTime += Time.deltaTime;
+
+        if( mTime >= mFrameDelay[ mCurFrame ] )
+        {
+            mCurFrame = ( mCurFrame + 1 ) % mFrames.Length;
+            mTime = 0.0f;
+
+            GetComponent<Renderer>().material.mainTexture = mFrames[mCurFrame];
+        }
     }
 }
 

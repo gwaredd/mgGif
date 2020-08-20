788b92a33dbd06f51f7268a368e46ebba2e67286
gwaredd <gwaredd@hotmail.com>
2020-08-20 12:29:25 +0100

    added test scene

---
+++ Assets/Scenes/AnimatedTextures.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class AnimatedTextures : MonoBehaviour
+{
+    public string Filename;
+
+    void Start()
+    {
+        //MG.GIF.Decoder.Parse()
+    }
+
+    void Update()
+    {
+    }
+}
+

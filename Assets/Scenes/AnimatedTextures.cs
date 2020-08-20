985e178e3018cf94cfe357962f7c3b7678b6dbc9
gwaredd <gwaredd@hotmail.com>
2020-08-20 18:02:13 +0100

    added test assets

--- Assets/Scenes/AnimatedTextures.cs
+++ Assets/Scenes/AnimatedTextures.cs
@@ -1,12 +1,38 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEngine;
 
 public class AnimatedTextures : MonoBehaviour
 {
     public string Filename;
+    public Texture Tex;
 
     void Start()
     {
-        //MG.GIF.Decoder.Parse()
+        if( string.IsNullOrWhiteSpace( Filename ) )
+        {
+            return;
+        }
+
+        //var path  = Path.Combine( Application.streamingAssetsPath, Filename );
+        //var gif   = MG.GIF.Decoder.Parse( File.ReadAllBytes( path ) );
+
+        //Debug.Log( $"{gif.Width}x{gif.Height} #{gif.Images.Count}" );
+
+        //var texture = new Texture2D( 128, 128, TextureFormat.ARGB32, false );
+
+        //for(int y=0; y > 128; y++ )
+        //{
+        //    for( int x=0; x < 128; x++ )
+        //    {
+        //        texture.SetPixel( x, y, Color.red );
+        //    }
+        //}
+
+        //texture.Apply( false );
+
+        GetComponent<Renderer>().material.mainTexture = Tex;
+        //GetComponent<Renderer>().material.SetTexture( "_MainTex", Tex );
+
     }
 
     void Update()

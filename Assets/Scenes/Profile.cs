f9ccda507ddeec4fd3be1efad55f9e4060d7096d
Gwaredd <gwaredd@hotmail.com>
2020-09-19 11:51:42 +0100

    added standard deviation to profile

--- Assets/Scenes/Profile.cs
+++ Assets/Scenes/Profile.cs
@@ -1,19 +1,25 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Profiling;
 
 public class Profile : MonoBehaviour
 {
     private int      mCount;
-    private long     mTotal;
-    private string[] mFiles;
+    private long     mSum;
+    private long     mSumSquares;
+    private byte[][] mFiles;
 
     void Start()
     {
-        mCount = 0;
-        mTotal = 0;
-        mFiles = Directory.GetFiles( Application.streamingAssetsPath, "*.gif", SearchOption.TopDirectoryOnly );
+        mCount      = 0;
+        mSum        = 0;
+        mSumSquares = 0;
+
+        var files = Directory.GetFiles( Application.streamingAssetsPath, "*.gif" );
+        mFiles = (from file in files select File.ReadAllBytes( file )).ToArray();
     }
 
     void Update()
@@ -25,20 +31,27 @@ public class Profile : MonoBehaviour
     {
         Profiler.BeginSample( $"Sample {sample}" );
 
+        var decoder = new MG.GIF.Decoder();
+
         var sw = new Stopwatch();
+
         sw.Start();
 
         foreach( var file in mFiles )
         {
-            var bytes = File.ReadAllBytes( file );
-            new MG.GIF.Decoder( bytes ).DecodeArray();
+            decoder.SetData( file );
+            decoder.GetImages();
         }
 
         sw.Stop();
-        mTotal += sw.ElapsedMilliseconds;
-        var avg = (float) mTotal / (float) mCount;
 
-        UnityEngine.Debug.Log( $"[{sample}]: avg <color=yellow>{avg:0.0}</color>ms ({sw.ElapsedMilliseconds}ms)" );
+        mSum += sw.ElapsedMilliseconds;
+        mSumSquares += sw.ElapsedMilliseconds * sw.ElapsedMilliseconds;
+
+        var average  = (float) mSum / mCount;
+        var variance = mSumSquares / mCount - average * average;
+
+        UnityEngine.Debug.Log( $"[{sample:00}]: av <color=yellow>{average:0.0}</color>ms, sd <color=yellow>{Mathf.Sqrt( variance ):0.0}</color> - {sw.ElapsedMilliseconds}ms" );
 
         Profiler.EndSample();
     }

1fc74496ca957fea1581d78016914e26cc5f4488
Gwaredd <gwaredd@hotmail.com>
2020-09-19 10:58:35 +0100

    better profile

--- Assets/Scenes/Profile.cs
+++ Assets/Scenes/Profile.cs
@@ -5,51 +5,20 @@ using UnityEngine.Profiling;
 
 public class Profile : MonoBehaviour
 {
-    public float SampleWaitTime = 0.25f;
-    public long  ThrottleWait   = 50;
-    public int   NumSamples     = 20;
-
-    private int     mCount;
-    private float   mTimer;
-    private long    mTotal;
+    private int      mCount;
+    private long     mTotal;
     private string[] mFiles;
 
     void Start()
     {
         mCount = 0;
-        mTimer = SampleWaitTime;
         mTotal = 0;
         mFiles = Directory.GetFiles( Application.streamingAssetsPath, "*.gif", SearchOption.TopDirectoryOnly );
     }
 
     void Update()
     {
-        if( mCount == NumSamples )
-        {
-            return;
-        }
-
-        mTimer -= Time.deltaTime;
-
-        Throttle();
-
-        if( mTimer < 0.0f )
-        {
-            Run( ++mCount );
-            mTimer = SampleWaitTime;
-        }
-    }
-
-    void Throttle()
-    {
-        var sw = new Stopwatch();
-        sw.Start();
-
-        while( sw.ElapsedMilliseconds < ThrottleWait )
-        {
-        }
-
-        sw.Stop();
+        Run( ++mCount );
     }
 
     void Run( int sample )
@@ -68,7 +37,8 @@ public class Profile : MonoBehaviour
         sw.Stop();
         mTotal += sw.ElapsedMilliseconds;
         var avg = (float) mTotal / (float) mCount;
-        UnityEngine.Debug.Log( $"Sample {sample}, {sw.ElapsedMilliseconds}ms, avg {avg}ms, {sw.ElapsedTicks} ticks" );
+
+        UnityEngine.Debug.Log( $"[{sample}]: avg <color=yellow>{avg:0.0}</color>ms ({sw.ElapsedMilliseconds}ms)" );
 
         Profiler.EndSample();
     }
